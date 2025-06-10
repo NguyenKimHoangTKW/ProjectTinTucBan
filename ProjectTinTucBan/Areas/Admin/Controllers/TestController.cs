52e3decf46fbfd779f5f7bd8450510ffd2ea92f5
Kim Hoang <hoanggnk.it@gmail.com>
2025-06-10 08:35:43 +0700

    note ngay gio kieu unix

--- ProjectTinTucBan/Areas/Admin/Controllers/TestController.cs
+++ ProjectTinTucBan/Areas/Admin/Controllers/TestController.cs
@@ -14,12 +14,32 @@ namespace ProjectTinTucBan.Areas.Admin.Controllers
     public class TestController : ApiController
     {
         WebTinTucTDMUEntities db = new WebTinTucTDMUEntities(); // mở cổng để gọi csdl với thuộc tính là db
+        private int unixTimestamp;
 
+        // Khai báo biến lấy giá trị ngày giờ kiểu int Unixtimestap
+        public TestController()
+        {
+            DateTime now = DateTime.UtcNow;
+            unixTimestamp = (int)(now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
         [HttpGet]
         [Route("test")] // tên của api ví dụ (api/v1/admin/test)
         public async Task<IHttpActionResult> test()
         {
             var get = db.BaiViets.ToListAsync(); // lấy ra toàn bộ record trong table BaiViets
+            return Ok();
+        }
+
+        // Tạo ngày giờ kiểu int unixtimestap
+        [HttpPost]
+        [Route("test-tao")]
+        public async Task<IHttpActionResult> tao_moi(BaiViet items)
+        {
+            var new_record = new BaiViet
+            {
+                NgayCapNhat = unixTimestamp, // gán ngày giờ kiểu unixtimestap vào
+                NgayDang = unixTimestamp // gán ngày giờ kiểu unixtimestap vào
+            };
             return Ok();
         }
     }
