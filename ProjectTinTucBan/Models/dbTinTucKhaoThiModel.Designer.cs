70bc5a169ad15bf3ccefae3f326c25fdd27ecaa5
unknown <hoanggnk.it@gmail.com>
2025-06-15 17:19:48 +0700

    Fix mot so loi vat khi merge code cua 4 thanh vien

--- ProjectTinTucBan/Models/dbTinTucKhaoThiModel.Designer.cs
+++ ProjectTinTucBan/Models/dbTinTucKhaoThiModel.Designer.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿// T4 code generation is enabled for model 'C:\Users\VinhNguyen090\Source\Repos\ProjectTinTucBan\ProjectTinTucBan\Models\dbTinTucKhaoThiModel.edmx'. 
-=======
-﻿// T4 code generation is enabled for model 'C:\Users\DELL\Source\Repos\ProjectTinTucBan\ProjectTinTucBan\Models\dbTinTucKhaoThiModel.edmx'. 
->>>>>>> Khiem
+﻿// T4 code generation is enabled for model 'D:\ProjectTinTucBan\ProjectTinTucBan\Models\dbTinTucKhaoThiModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
